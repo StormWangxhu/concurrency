93f82c959a03525805dcfafdd0cca7f88f401e94
StormWangxhu <StormWangxhu@163.com>
2019-01-07 13:08:18 +0800

    Count计数，线程安全AtomicLong

---
+++ src/main/java/com/wangxhu/concurrency/example/atomic/AtomicExample2.java
@@ -0,0 +1,54 @@
+package com.wangxhu.concurrency.example.atomic;
+
+import com.wangxhu.concurrency.annoations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Slf4j
+@ThreadSafe
+public class AtomicExample2 {
+
+    // 请求总数
+    public static int clientTotal = 5000;
+
+    // 同时并发执行的线程数
+    public static int threadTotal = 200;
+
+    public static AtomicLong count = new AtomicLong(0);
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal ; i++) {
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    add();
+                    semaphore.release();
+                } catch (Exception e) {
+//                    log.error("exception", e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+//        log.info("count:{}", count.get());
+        System.out.println(count);
+    }
+
+    private static void add() {
+        /**
+         * AtomicInteger.incrementAndGet()方法的原子性
+         */
+        count.incrementAndGet();
+        // count.getAndIncrement();
+    }
+}
