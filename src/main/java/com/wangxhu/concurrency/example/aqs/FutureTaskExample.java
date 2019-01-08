cbb7b02a9c7bc3435f8ae5639afdbab2e39bead7
StormWangxhu <StormWangxhu@163.com>
2019-01-08 15:52:08 +0800

    FutureTask 既可以使得线程做事情，还可以得到线程执行的结果

---
+++ src/main/java/com/wangxhu/concurrency/example/aqs/FutureTaskExample.java
@@ -0,0 +1,27 @@
+package com.wangxhu.concurrency.example.aqs;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+
+@Slf4j
+public class FutureTaskExample {
+
+    public static void main(String[] args) throws Exception {
+        FutureTask<String> futureTask = new FutureTask<String>(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                System.out.println("do something in callable");
+                Thread.sleep(5000);
+                return "Done";
+            }
+        });
+
+        new Thread(futureTask).start();
+        System.out.println("do something in main");
+        Thread.sleep(1000);
+        String result = futureTask.get();
+        System.out.println("result："+result);
+    }
+}
