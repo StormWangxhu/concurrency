505ff85657e71a245d8a08e7833b5ef71102cc12
StormWangxhu <StormWangxhu@163.com>
2019-01-08 17:11:57 +0800

    Fork/Join 框架 分而治之核心思想

---
+++ src/main/java/com/wangxhu/concurrency/example/aqs/ForkJoinTaskExample.java
@@ -0,0 +1,77 @@
+package com.wangxhu.concurrency.example.aqs;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.Future;
+import java.util.concurrent.RecursiveTask;
+
+
+/**
+ * Fork/Join算法核心思想： 分而治之
+ */
+@Slf4j
+public class ForkJoinTaskExample extends RecursiveTask<Integer> {
+
+    public static final int threshold = 2;//阀值
+    private int start;
+    private int end;
+
+    public ForkJoinTaskExample(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    /**
+     * compute: 计算
+     *
+     * @return
+     */
+    @Override
+    protected Integer compute() {
+        int sum = 0;
+
+        //如果任务足够小就计算任务
+        boolean canCompute = (end - start) <= threshold;
+        if (canCompute) {
+            for (int i = start; i <= end; i++) {
+                sum += i;
+            }
+        } else {
+            // 如果任务大于阈值，就分裂成两个子任务计算
+            int middle = (start + end) / 2;
+            ForkJoinTaskExample leftTask = new ForkJoinTaskExample(start, middle);
+            ForkJoinTaskExample rightTask = new ForkJoinTaskExample(middle + 1, end);
+
+            // 执行子任务
+            leftTask.fork();
+            rightTask.fork();
+
+            // 等待任务执行结束合并其结果
+            int leftResult = leftTask.join();
+            int rightResult = rightTask.join();
+
+            // 合并子任务
+            sum = leftResult + rightResult;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+
+        //创建一个线程池
+        ForkJoinPool forkjoinPool = new ForkJoinPool();
+
+        //生成一个计算任务，计算1+2+3+4
+        ForkJoinTaskExample task = new ForkJoinTaskExample(1, 100);
+
+        //将任务提交到线程池中,执行一个任务
+        Future<Integer> result = forkjoinPool.submit(task);
+
+        try {
+            System.out.println("result:" + result.get());
+        } catch (Exception e) {
+            System.out.println("exception" + e);
+        }
+    }
+}
