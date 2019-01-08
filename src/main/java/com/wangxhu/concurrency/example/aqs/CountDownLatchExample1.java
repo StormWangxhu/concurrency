75b554658a215c5adae6f93dc541960a25e33743
StormWangxhu <StormWangxhu@163.com>
2019-01-08 14:38:22 +0800

    AQS&CountDownLatch

---
+++ src/main/java/com/wangxhu/concurrency/example/aqs/CountDownLatchExample1.java
@@ -0,0 +1,43 @@
+package com.wangxhu.concurrency.example.aqs;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Slf4j
+public class CountDownLatchExample1 {
+
+    private final static int threadCount = 200;
+
+    public static void main(String[] args) throws Exception {
+
+        //线程池
+        ExecutorService exec = Executors.newCachedThreadPool();
+
+        final CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            final int threadNum = i;
+            exec.execute(() -> {
+                try {
+                    test(threadNum);
+                } catch (Exception e) {
+
+                } finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+        countDownLatch.await();
+        System.out.println("finish");
+        exec.shutdown();
+    }
+
+    private static void test(int threadNum) throws Exception {
+        Thread.sleep(1000);
+        System.out.println(threadNum);
+        Thread.sleep(1000);
+    }
+}
